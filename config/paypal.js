56ffc79bc62fcdf3c425e9a9f6cc8204b8addc05
isabuhay013 <john.nico.austria@gmail.com>
2019-08-03 05:14:47 -0700

    add urls in paypal

--- config/paypal.js
+++ config/paypal.js
@@ -1,6 +1,6 @@
 module.exports = {
   client_id : 'AQKFyQ5slFTCzJK4ptOlf-KApv1K4eczFM5ILeifN9FXLs8amP-k9uTj9Msj9LgTUwPbDMFTnhuDw3ZM',
   client_secret : 'EFLnIV3aRBUu3clfkoeQZK3YwaJ3EsZwTZGfTCXXtN9yoer42rptHtt7onnDDTilB4bq6NL9qlH59fvZ',
-  success_url : 'http://localhost:2000/cart/success',
-  cancel_url : 'http://localhost:2000/cart/cancel'
+  success_url : 'https://serene-temple-72168.herokuapp.com/cart/success',
+  cancel_url : 'https://serene-temple-72168.herokuapp.com/cart/cancel'
 }
