2028e627ce8eb597f1465ce583b246b9d85de265
isabuhay013 <john.nico.austria@gmail.com>
2019-08-03 06:04:07 -0700

    commit 2

--- config/paypal.js
+++ config/paypal.js
@@ -1,6 +1,12 @@
 module.exports = {
   client_id : 'AQKFyQ5slFTCzJK4ptOlf-KApv1K4eczFM5ILeifN9FXLs8amP-k9uTj9Msj9LgTUwPbDMFTnhuDw3ZM',
   client_secret : 'EFLnIV3aRBUu3clfkoeQZK3YwaJ3EsZwTZGfTCXXtN9yoer42rptHtt7onnDDTilB4bq6NL9qlH59fvZ',
-  success_url : 'https://serene-temple-72168.herokuapp.com/cart/success',
-  cancel_url : 'https://serene-temple-72168.herokuapp.com/cart/cancel'
+
+  // dev
+  // success_url : 'http://localhost:2000/cart/success',
+  // cancel_url : 'http://localhost:2000/cart/cancel',
+
+  // prod
+  success_url : 'https://intense-eyrie-63357.herokuapp.com/cart/success',
+  cancel_url : 'https://intense-eyrie-63357.herokuapp.com/cart/cancel'
 }
