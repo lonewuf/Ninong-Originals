b9813be19f3c13ce6388d95d44a012c24eafa821
isabuhay013 <john.nico.austria@gmail.com>
2019-09-02 23:15:40 -0700

    add db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://localhost/ggadget-dev'
-  // database: 'mongodb://isabuhay:l0n3wolf@ds133086.mlab.com:33086/gulapagadgets-dev'
+  databaseDev: 'mongodb://localhost/ggadget-dev',
+  databaseProd: 'mongodb://isabuhay:l0n3wolf@ds133086.mlab.com:33086/gulapagadgets-dev'
 }
